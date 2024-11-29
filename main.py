17dd3776638d0abe80db56f34c396e577a410fcd
Logan Schmillen <schmillen20@gmail.com>
2024-11-29 16:29:10 -0600

    added tts and logic to change voice

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 #pip install SpeechRecognition
 #pip install pyttsx3
 #pip install PyAudio
+
+#imports
 import speech_recognition as sr
+import pyttsx3 as tts
 
-
+#declaring objects
 listener = sr.Recognizer()
-print("Recognizer initialized.")
+engine = tts.init()
 
+#Where to change voice engine
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+#Synthesis
 try:
     print("Attempting to use microphone...")
     with sr.Microphone() as source:
@@ -14,6 +22,8 @@ try:
         voice = listener.listen(source, timeout=5, phrase_time_limit=10)
         print("Voice captured. Recognizing...")
         command = listener.recognize_google(voice)
-        print(f"Command: {command}")
+        
+        engine.say(command)
+        engine.runAndWait()
 except Exception as e:
     print(f"An error occurred: {e}")
